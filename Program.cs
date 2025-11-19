5ea677809abeaee4a3716f85f23128651a3e55d5
Roger de Paula <roger.paula@chipcia.com.br>
2025-11-19 01:57:55 -0300

    Add Swagger setup for API documentation and exploration

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -16,7 +18,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{   
+    app.UseSwagger();            // gera /swagger/v1/swagger.json
+    app.UseSwaggerUI();          // gera a UI em /swagger
     app.MapOpenApi();
 }
 
